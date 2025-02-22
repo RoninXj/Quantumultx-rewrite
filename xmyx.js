ef58e3f6f10c760b09c62ba4af57723f30b082be
RoninXj <48503899+RoninXj@users.noreply.github.com>
2025-02-22 22:31:29 +0800

    Update xmyx.js

--- xmyx.js
+++ xmyx.js
@@ -9,7 +9,7 @@ new Env('星妈优选');
 图标： https://raw.githubusercontent.com/leiyiyan/resource/main/icons/xmyx.png
 
 [Script]
-http-response ^https?:\/\/www\.feihevip\.com\/api\/starMember\/getMemberInfo script-path=https://raw.githubusercontent.com/RoninXj/Quantumultx-rewrite/rewrite/xmyx.js, requires-body=true, timeout=60, tag=星妈优选获取Cookie
+http-response ^https?:\/\/www\.feihevip\.com\/api\/starMember\/getMemberServe script-path=https://raw.githubusercontent.com/RoninXj/Quantumultx-rewrite/rewrite/xmyx.js, requires-body=true, timeout=60, tag=星妈优选获取Cookie
 cron "30 0 * * *" script-path=https://raw.githubusercontent.com/RoninXj/Quantumultx-rewrite/rewrite/xmyx.js, tag=星妈优选日常任务
 
 [MITM]
